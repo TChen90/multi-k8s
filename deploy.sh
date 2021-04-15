525cf804901c4b49042736cbc745bd257b353228
Tianli Chen <tchen@tianlis-mbp.propark.com>
2021-04-15 13:10:06 -0400

    added deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,15 @@
-docker build -t cygnetops/multi-client-k8s:latest -t cygnetops/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
-docker build -t cygnetops/multi-server-k8s-pgfix:latest -t cygnetops/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
-docker build -t cygnetops/multi-worker-k8s:latest -t cygnetops/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+docker build -t tchen90pro/multi-client:latest -t tchen90pro/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t tchen90pro/multi-server:latest -t tchen90pro/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t tchen90pro/multi-worker:latest -t tchen90pro/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push tchen90pro/multi-client:latest
+docker push tchen90pro/multi-server:latest
+docker push tchen90pro/multi-worker:latest
 
-docker push cygnetops/multi-client-k8s:latest
-docker push cygnetops/multi-server-k8s-pgfix:latest
-docker push cygnetops/multi-worker-k8s:latest
-
-docker push cygnetops/multi-client-k8s:$SHA
-docker push cygnetops/multi-server-k8s-pgfix:$SHA
-docker push cygnetops/multi-worker-k8s:$SHA
+docker push tchen90pro/multi-client:$SHA
+docker push tchen90pro/multi-server:$SHA
+docker push tchen90pro/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=cygnetops/multi-server-k8s-pgfix:$SHA
-kubectl set image deployments/client-deployment client=cygnetops/multi-client-k8s:$SHA
-kubectl set image deployments/worker-deployment worker=cygnetops/multi-worker-k8s:$SHA
+kubectl set image deployments/server-deployment server=tchen90pro/multi-server:$SHA
+kubectl set image deployments/client-deployment client=tchen90pro/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=tchen90pro/multi-worker:$SHA
